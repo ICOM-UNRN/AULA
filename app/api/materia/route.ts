a436010d5f84c5cbb711bb22c9112b24b2ba17b7
ICOM-UNRN <ingenieriaencomputacion@unrn.edu.ar>
2024-06-07 14:26:50 -0300

    Update materia/route.ts

--- app/api/materia/route.ts
+++ app/api/materia/route.ts
@@ -105,7 +105,7 @@ export async function GET(request: Request) {
     if (error instanceof Error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
     } else {
-      return NextResponse.json({ error: "Unknown error occurred" }, { status: 500 });
+      return NextResponse.json({ error: 'Unknown error occurred' }, { status: 500 });
     }
   }
 }
